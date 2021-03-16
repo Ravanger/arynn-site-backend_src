a7c93fcb4d5b52d3a593fb0f805d45ddc067ebf4
Ravanger <Boris.Rozzy@gmail.com>
2021-03-16 16:02:53 -0400

    Sentry config, test crash

--- middlewares/sentry/index.js
+++ middlewares/sentry/index.js
@@ -1,10 +1,15 @@
 const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn:
+    "https://22b6cc02e1d144998b2d69808fb04f5a@o553036.ingest.sentry.io/5679710",
+  integrations: [new Tracing.Integrations.BrowserTracing()],
   tracesSampleRate: 1.0,
   environment: strapi.config.environment,
 });
+
+myUndefinedFunction();
 
 module.exports = (strapi) => {
   return {
