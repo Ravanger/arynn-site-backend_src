4612f46f5e9c712b3d6cf77620108c9c0728a15b
Ravanger <Boris.Rozzy@gmail.com>
2021-03-16 15:26:05 -0400

    Add Sentry error monitoring

---
+++ middlewares/sentry/index.js
@@ -0,0 +1,22 @@
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  environment: strapi.config.environment,
+});
+
+module.exports = (strapi) => {
+  return {
+    initialize() {
+      strapi.app.use(async (ctx, next) => {
+        try {
+          await next();
+        } catch (error) {
+          Sentry.captureException(error);
+          throw error;
+        }
+      });
+    },
+  };
+};
